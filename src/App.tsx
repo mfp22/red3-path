fba2eb9786fc55427890a7bd68e0b1b045e40250
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-21 16:42:35 -0700

    Add @reach/router instead of react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import './App.css';
+import { RouteComponentProps, Router } from '@reach/router';
+
+function PageA(props: RouteComponentProps) {
+    return (
+        <div className="">A</div>
+    );
+}
+
+function PageB(props: RouteComponentProps) {
+    return (
+        <div className="">2222</div>
+    );
+}
 
 function App() {
     return (
@@ -7,6 +20,10 @@ function App() {
             <header className="bg-gray-600">
                 123
             </header>
+            <Router>
+                <PageA path="/" />
+                <PageB path="b"/>
+            </Router>
         </div>
     );
 }
