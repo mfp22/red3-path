db108bb11a944b8159828aa1601a984a2903eee4
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-21 02:52:17 -0700

    Initial add tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <div className="">
-            <header className="">
+            <header className="bg-gray-600">
                 123
             </header>
         </div>
