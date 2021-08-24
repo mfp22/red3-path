961d9f15156a80d4167b184f1156366a4f611e85
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-24 14:08:28 -0700

    WIP. Slider. now it works w/ js variables

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -1,21 +1,21 @@
 import { styled } from '@stitches/react';
 
+const __rangeHandleColor = "#ffffff";      // !default
+const __rangeHandleColorHover = "#907aff54";       // !default
+const __rangeHandleSize = "'12px'"; // def 16px        // !default
+const __rangeHandleRadius = "25%"; // def 50%
+
+const __rangeTrackColor = "#6c48f04d";     // !default
+const __rangeTrackHeight = "1px"; // def 2px       // !default
+const __rangeTrackBorderColor = "rgba(108; 72; 240; 0.5)";     // !default
+
 export const Slider = styled('input', {
     //'input[type = "range"].ui - slider': {
-        $$rangeHandleColor: "#ffffff",      // !default
-        $$rangeHandleColorhover: "#907aff54",       // !default
-        $$rangeHandleSize: "'12px'", // def 16px        // !default
-        $$rangeHandleRadius: "25%", // def 50%
-
-        $$rangeTrackColor: "#6c48f04d",     // !default
-        $$rangeTrackHeight: "1px", // def 2px       // !default
-        $$rangeTrackBorderColor: "rgba(108, 72, 240, 0.5)",     // !default
-
         WebkitAppearance: "none",
-        width: "100%", //$$range-label-width: 60px !default; width: calc(100% - (#{$$range-label-width + 10px}));
-        height: "$$rangeTrackHeight",
+        width: "100%", //__range-label-width: 60px !default; width: calc(100% - (#{__range-label-width + 10px}));
+        height: __rangeTrackHeight,
         borderRadius: "5px",
-        background: "$$rangeTrackColor",
+        background: __rangeTrackColor,
         outline: "none",
         padding: "0",
         margin: "0",
@@ -24,36 +24,35 @@ export const Slider = styled('input', {
         // Range Handle
         '&::-webkit-slider-thumb': {
             WebkitAppearance: "none",
-            width: "$$rangeHandleSize",
-            height: "$$rangeHandleSize",
-            borderRadius: "$$rangeHandleRadius",
-            background: "$$rangeHandleColor",
+            width: __rangeHandleSize,
+            height: __rangeHandleSize,
+            borderRadius: __rangeHandleRadius,
+            background: __rangeHandleColor,
             cursor: "pointer",
             transition: "all 0.15s ease-in-out",
-            border: "1px solid $$rangeTrackColor",
-            borderBottom: "1px solid $$rangeTrackBorderColor",
-            borderRight: "1px solid $$rangeTrackBorderColor",
+            border: `1px solid ${__rangeTrackColor}`,
+            borderBottom: `1px solid ${__rangeTrackBorderColor}`,
+            borderRight: `1px solid ${__rangeTrackBorderColor}`,
             '&:hover': {
-                background: "$$rangeHandleColorHover",
+                background: __rangeHandleColorHover,
                 transform: "scale(1.2)",
                 border: "1px solid white"
             }
         },
         '&::-moz-range-thumb': {
-            width: "$$rangeHandleSize",
-            height: "$$rangeHandleSize",
+            width: __rangeHandleSize,
+            height: __rangeHandleSize,
             border: "0",
-            borderRadius: "$$rangeHandleRadius",
-            background: "$$rangeHandleColor",
+            borderRadius: __rangeHandleRadius,
+            background: __rangeHandleColor,
             cursor: "pointer",
             transition: "background 0.15s ease-in-out",
-            borderBottom: "1px solid $$rangeTrackBorderColor",
-            borderRight: "1px solid $$rangeTrackBorderColor",
+            borderBottom: `1px solid ${__rangeTrackBorderColor}`,
+            borderRight: `1px solid ${__rangeTrackBorderColor}`,
             '&:hover': {
-                background: "$$rangeHandleColorHover",
+                background: __rangeHandleColorHover,
                 border: "1px solid white"
             }
         },
     //}
 });
-
