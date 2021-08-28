efde376d0a1de3af940a3cb5dab05f878bea75ec
Max Zakharzhevskiy <maxzz@msn.com>
2021-08-28 01:05:05 -0700

    WIP. slider balloon. track activation OK

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -38,6 +38,7 @@ const SliderRange = styled(RadixSlider.Range, {
 
 const SliderThumb = styled(RadixSlider.Thumb, {
     ...defaultStyles,
+    position: 'relative',
     display: 'flex',
     width: 18,
     height: 18,
@@ -45,9 +46,34 @@ const SliderThumb = styled(RadixSlider.Thumb, {
     borderRadius: '50%',
     cursor: 'pointer',
     outline: '1px solid #00000050',
+    '--active': 0,
     '&:hover': { backgroundColor: 'var(--tm-primary-500)' },
+    '&:active': { '--active': 1 },
     '&:focus': { boxShadow: '0 0 0 5px #0000001c' },
 });
+
+const SliderBalloon = styled('div', {
+    position: 'absolute',
+    left: 0,
+    //top: 0,
+    bottom: '3px',
+    width: '32px',
+    height: '62px',
+    //padding: '8px 3px 0',
+    margin: '0 0 0 -7px',
+    //backgroundColor: 'red',
+    transform: 'scale(var(--active))',
+    transition: 'transform .2s ease',
+    transformOrigin: '50% 90%',
+});
+
+function BallonSVG() {
+    return (
+        <svg width="32" height="62" fill="var(--tm-primary-500)" stroke="#00000050">
+            <path d="M27.3 27.3C25 29.6 17 35.8 17 43v3c0 3 2.5 5 3.2 5.8a6 6 0 1 1-8.5 0C12.6 51 15 49 15 46v-3c0-7.2-8-13.4-10.3-15.7A16 16 0 0 1 16 0a16 16 0 0 1 11.3 27.3z" />
+        </svg>
+    );
+};
 
 const Slider = React.forwardRef<HTMLSpanElement, RadixSlider.SliderOwnProps & { ariaLabel?: string; }>((props, forwardedRef) => {
     const value = props.value || props.defaultValue || [];
@@ -58,7 +84,10 @@ const Slider = React.forwardRef<HTMLSpanElement, RadixSlider.SliderOwnProps & { 
                 <SliderRange />
             </SliderTrack>
             {value.map((_, i) => <SliderThumb key={i} aria-label={ariaLabel}>
-            <div className="w-32 h-32 bg-red-500 hidden active:block scale-[.2] active:scale-1"></div>
+                {/* <div className="w-32 h-32 bg-red-500 hidden active:block scale-[.2] active:scale-1"></div> */}
+                <SliderBalloon>
+                    <BallonSVG />
+                </SliderBalloon>
             </SliderThumb>)}
         </SliderRoot>
     );
