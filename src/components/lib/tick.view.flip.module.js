ff23e5a93133a372be0fd4a54d7165439e132950
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-01 02:32:16 -0700

    WIP. points count display. remove unused asyncGenerator

--- src/components/lib/tick.view.flip.module.js
+++ src/components/lib/tick.view.flip.module.js
@@ -10,123 +10,6 @@ export default typeof window !== 'undefined' ? (function () {
     }
     'use strict';
 
-    var asyncGenerator = function () {
-        function AwaitValue(value) {
-            this.value = value;
-        }
-
-        function AsyncGenerator(gen) {
-            var front, back;
-
-            function send(key, arg) {
-                return new Promise(function (resolve, reject) {
-                    var request = {
-                        key: key,
-                        arg: arg,
-                        resolve: resolve,
-                        reject: reject,
-                        next: null
-                    };
-
-                    if (back) {
-                        back = back.next = request;
-                    } else {
-                        front = back = request;
-                        resume(key, arg);
-                    }
-                });
-            }
-
-            function resume(key, arg) {
-                try {
-                    var result = gen[key](arg);
-                    var value = result.value;
-
-                    if (value instanceof AwaitValue) {
-                        Promise.resolve(value.value).then(function (arg) {
-                            resume("next", arg);
-                        }, function (arg) {
-                            resume("throw", arg);
-                        });
-                    } else {
-                        settle(result.done ? "return" : "normal", result.value);
-                    }
-                } catch (err) {
-                    settle("throw", err);
-                }
-            }
-
-            function settle(type, value) {
-                switch (type) {
-                    case "return":
-                        front.resolve({
-                            value: value,
-                            done: true
-                        });
-                        break;
-
-                    case "throw":
-                        front.reject(value);
-                        break;
-
-                    default:
-                        front.resolve({
-                            value: value,
-                            done: false
-                        });
-                        break;
-                }
-
-                front = front.next;
-
-                if (front) {
-                    resume(front.key, front.arg);
-                } else {
-                    back = null;
-                }
-            }
-
-            this._invoke = send;
-
-            if (typeof gen.return !== "function") {
-                this.return = undefined;
-            }
-        }
-
-        if (typeof Symbol === "function" && Symbol.asyncIterator) {
-            AsyncGenerator.prototype[Symbol.asyncIterator] = function () {
-                return this;
-            };
-        }
-
-        AsyncGenerator.prototype.next = function (arg) {
-            return this._invoke("next", arg);
-        };
-
-        AsyncGenerator.prototype.throw = function (arg) {
-            return this._invoke("throw", arg);
-        };
-
-        AsyncGenerator.prototype.return = function (arg) {
-            return this._invoke("return", arg);
-        };
-
-        return {
-            wrap: function (fn) {
-                return function () {
-                    return new AsyncGenerator(fn.apply(this, arguments));
-                };
-            },
-            await: function (value) {
-                return new AwaitValue(value);
-            }
-        };
-    }();
-
-
-
-
-
     var classCallCheck = function (instance, Constructor) {
         if (!(instance instanceof Constructor)) {
             throw new TypeError("Cannot call a class as a function");
@@ -407,8 +290,8 @@ export default typeof window !== 'undefined' ? (function () {
             createClass(FlipCard, [{
                 key: 'rotate',
                 value: function rotate(degrees) {
-                    this._front.style.transform = 'rotateX(' + degrees + 'deg)';
-                    this._back.style.transform = 'rotateX(' + (-180 + degrees) + 'deg)';
+                    this._front.style.transform = `rotateX(${degrees}deg)`;
+                    this._back.style.transform = `rotateX(${-180 + degrees}deg)`;
                 }
             }, {
                 key: 'root',
@@ -449,18 +332,15 @@ export default typeof window !== 'undefined' ? (function () {
                     this._shadowFront.style.opacity = value;
                 }
             }]);
+
             return FlipCard;
         }();
 
         /**
-      * Expose
-      */
-
-
+         * Expose
+         */
         return function (root) {
-
             var state = {
-
                 cards: [],
                 lastCard: null,
                 initialCard: null,
@@ -476,10 +356,13 @@ export default typeof window !== 'undefined' ? (function () {
                 back: null
             };
 
-            return Object.assign({}, rooter(state, root, 'flip'), updater(state), styler(state, {
-                flipDuration: 800,
-                flipEasing: 'ease-out-bounce'
-            }), drawer(state, draw), destroyer(state));
+            return Object.assign({},
+                rooter(state, root, 'flip'),
+                updater(state),
+                styler(state, { flipDuration: 800, flipEasing: 'ease-out-bounce' }),
+                drawer(state, draw),
+                destroyer(state),
+            );
         };
     });
 
