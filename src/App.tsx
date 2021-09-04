4785ffda6d082b0774f47667e620e0da05070140
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-04 16:08:54 -0700

    Add app footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,28 @@ import React from 'react';
 import './App.css';
 import { PathSimplifyProvider } from './store/PathSimplify';
 import PathSimplifyReact from './components/PathSimplifyReact';
+import Footer from './components/ui/Footer';
 
 function App() {
     return (
-        <PathSimplifyProvider>
-            <div className="h-screen flex flex-col">
-                <header className="flex-none p-2 shadow-md">
-                    <div className="">
-                        <div className="space-x-2 flex items-center">
-                            <div className="block p-2 rounded text-gray-100 bg-primary-700 saturate-50 border border-primary-400 shadow">Simplify points</div>
-                            <div className="block p-2 rounded text-gray-100 bg-primary-700 saturate-50 border border-primary-400 shadow">Simplify path</div>
+        <>
+            <PathSimplifyProvider>
+                <div className="h-screen flex flex-col">
+                    <header className="flex-none p-2 shadow-md">
+                        <div className="">
+                            <div className="space-x-2 flex items-center">
+                                <div className="block p-2 rounded text-gray-100 bg-primary-700 saturate-50 border border-primary-400 shadow">Simplify points</div>
+                                <div className="block p-2 rounded text-gray-100 bg-primary-700 saturate-50 border border-primary-400 shadow">Simplify path</div>
+                            </div>
                         </div>
+                    </header>
+                    <div className="flex-1 lg:max-w-5xl mx-auto flex items-center justify-center">
+                        <PathSimplifyReact />
                     </div>
-                </header>
-                <div className="flex-1 lg:max-w-5xl mx-auto flex items-center justify-center">
-                    <PathSimplifyReact />
                 </div>
-            </div>
-        </PathSimplifyProvider >
+            </PathSimplifyProvider >
+            <Footer />
+        </>
     );
 }
 
