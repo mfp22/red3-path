3abf62f9ed756bdc08d0d83971c8b5e876e20046
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-04 17:43:28 -0700

    Update Hero layout

--- src/components/ui/HeroInfo.tsx
+++ src/components/ui/HeroInfo.tsx
@@ -4,7 +4,7 @@ function HeroInfo({className, style, ...rest}: React.HTMLAttributes<HTMLDivEleme
 
     return (
         <div 
-            className={`py-2 hidden sm:block text-sm md:text-lg lg:text-xl text-primary-900 tracking-tight ${className}`}
+            className={`text-primary-900 tracking-tight ${className}`}
             style={{ textShadow: '1px 1px #5a5a5a', ...style }}
             {...rest}
         >
