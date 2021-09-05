ab0481ea8207b082323d7f3c0bb5adc6c3e1e999
Max Zakharzhevskiy <maxzz@msn.com>
2021-09-04 19:51:11 -0700

    Update readme

--- src/components/PathSimplifyReact.tsx
+++ src/components/PathSimplifyReact.tsx
@@ -146,7 +146,7 @@ const PathSimplifyReact: React.FC<PathSimplifyReactProps> = () => {
             {/* Hero and instructions */}
             <div className="col-span-full my-0 md:my-4 lg:my-0 hidden sm:grid auto-cols-fr">
                 <Hero className="h-12 md:h-16 text-primary-900 opacity-75 md:opacity-50" />
-                <HeroInfo className="text-sm md:text-lg text-primary-200 opacity-90" />
+                <HeroInfo className="text-sm md:text-lg text-primary-100 opacity-90" />
             </div>
 
             {/* Canvas */}
