ba9cf9ef6a40996c9c3164ba83b87b77ea934b62
Max Zakharzhevskiy <maxzz@msn.com>
2022-07-02 02:08:30 -0700

    WIP. app. split PathSimplifyReact into parts

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,10 @@ import PathSimplifyReact from './components/PathSimplifyReact';
 import Footer from './components/ui/Footer';
 
 function App() {
-    return (
-        <>
-            <PathSimplifyProvider>
-                <div className="h-screen flex flex-col">
-                    {/* <header className="p-2 shadow-md">
+    return (<>
+        <PathSimplifyProvider>
+            <div className="h-screen flex flex-col">
+                {/* <header className="p-2 shadow-md">
                         <div className="">
                             <div className="space-x-2 flex items-center">
                                 <div className="block p-2 rounded text-gray-100 bg-primary-700 saturate-50 border border-primary-400 shadow">Simplify points</div>
@@ -17,17 +16,15 @@ function App() {
                             </div>
                         </div>
                     </header> */}
-                    <div className="flex-1 lg:max-w-5xl mx-auto px-4 flex items-center justify-center">
-                        <PathSimplifyReact />
-                    </div>
-                    <div className="hidden sm:block">
-                        <Footer />
-                    </div>
+                <div className="flex-1 lg:max-w-5xl mx-auto px-4 flex items-center justify-center">
+                    <PathSimplifyReact />
                 </div>
-            </PathSimplifyProvider >
-            
-        </>
-    );
+                <div className="hidden sm:block">
+                    <Footer />
+                </div>
+            </div>
+        </PathSimplifyProvider >
+    </>);
 }
 
 export default App;
--- src/components/PathSimplifyReact.tsx
+++ src/components/PathSimplifyReact.tsx
@@ -187,7 +187,42 @@ function SliderPrecision() {
     );
 }
 
-const PathSimplifyReact: React.FC<PathSimplifyReactProps> = () => {
+function Header() {
+    return (
+        <div className="col-span-full my-0 md:my-4 lg:my-0 hidden sm:grid auto-cols-fr">
+            <Hero className="h-12 md:h-16 text-primary-900 opacity-75 md:opacity-50" />
+            <HeroInfo className="text-sm md:text-lg text-primary-100 opacity-90" />
+        </div>
+    );
+}
+
+function Controls({ points, setPoints, controlPoints }: {
+    points: [number, number][]; setPoints: React.Dispatch<React.SetStateAction<[number, number][]>>; controlPoints: {
+        points: XY[];
+        controls: ControlPoint[];
+    };
+}) {
+    return (<>
+        <div className="lg:min-w-[20rem] p-4 space-y-4 bg-primary-300 text-sm border-primary-600 border-8 border-opacity-50">
+            <SliderTolerance />
+            <SliderStepPoints />
+            {/* <SliderPrecision /> */}
+
+            <div className="flex justify-between">
+                <ToggleButtons />
+                <button className="p-1 border border-primary-700 rounded shadow active:scale-[.97]" onClick={() => setPoints([])} title="Clear canvas points">Clear</button>
+            </div>
+        </div>
+
+        {/* In and out points stats */}
+        <div className="col-span-full">
+            <ResultDisplayProduction pointsSrc={points.length} pointsDst={controlPoints.points.length} />
+            {/* <Result pointsSrc={points.length} pointsDst={controlPoints.points.length} /> */}
+        </div>
+    </>);
+}
+
+function Editor() {
     const svgWidth = 500;
     const svgHeight = 500;
 
@@ -196,7 +231,7 @@ const PathSimplifyReact: React.FC<PathSimplifyReactProps> = () => {
 
     const addPoint = useCallback(debounce((pt: [number, number]) => setPoints(prev => [...prev, pt]), 50), []);
 
-    const bind = useDrag(({event, dragging, buttons}) => {
+    const bind = useDrag(({ event, dragging, buttons }) => {
         //if (event.event.type === 'pointerdown') {}
 
         if (dragging && buttons === 1) {
@@ -218,47 +253,36 @@ const PathSimplifyReact: React.FC<PathSimplifyReactProps> = () => {
     const controlPoints = React.useMemo(() => getPathPoints(path), [path]);
     const stepPoints = React.useMemo(() => svgCalcStepPoints(path, nStepPoints, svgWidth, svgHeight), [path, nStepPoints]);
 
+    return (<>
+        <div className="col-span-1 lg:col-span-2">
+            <svg ref={svgRef} {...bind()} viewBox={`0 0 ${svgWidth} ${svgHeight}`}
+                className="w-full h-full bg-primary-300 border-primary-600 border-8 border-opacity-50 touch-none"
+            >
+                {showPts && <RenderCpts pts={controlPoints.points} />}
+                {showCtr && <RenderCptsHandlesSquares cpts={controlPoints.controls} />}
+                {showRaw && <RenderRawPoints pts={points} />}
+                {showLine && <>
+                    <path fill="none" stroke={COLORS.slineLower} strokeWidth={SIZES.wLineLower} d={path} />
+                    <path fill="none" stroke={COLORS.slineUpper} strokeWidth={SIZES.wLineUpper} d={path} />
+                </>}
+                <RenderStepRawPoints pts={stepPoints} />
+            </svg>
+        </div>
+        <Controls points={points} setPoints={setPoints} controlPoints={controlPoints} />
+    </>);
+}
+
+const PathSimplifyReact: React.FC<PathSimplifyReactProps> = () => {
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 max-w-[420px] md:max-w-[480px] lg:max-w-full gap-4 text-gray-700 select-none">
             {/* Hero and instructions */}
-            <div className="col-span-full my-0 md:my-4 lg:my-0 hidden sm:grid auto-cols-fr">
-                <Hero className="h-12 md:h-16 text-primary-900 opacity-75 md:opacity-50" />
-                <HeroInfo className="text-sm md:text-lg text-primary-100 opacity-90" />
-            </div>
+            <Header />
 
             {/* Canvas */}
-            <div className="col-span-1 lg:col-span-2">
-                <svg ref={svgRef} {...bind()} viewBox={`0 0 ${svgWidth} ${svgHeight}`}
-                    className="w-full h-full bg-primary-300 border-primary-600 border-8 border-opacity-50 touch-none"
-                >
-                    {showPts && <RenderCpts pts={controlPoints.points} />}
-                    {showCtr && <RenderCptsHandlesSquares cpts={controlPoints.controls} />}
-                    {showRaw && <RenderRawPoints pts={points} />}
-                    {showLine && <>
-                        <path fill="none" stroke={COLORS.slineLower} strokeWidth={SIZES.wLineLower} d={path} />
-                        <path fill="none" stroke={COLORS.slineUpper} strokeWidth={SIZES.wLineUpper} d={path} />
-                    </>}
-                    <RenderStepRawPoints pts={stepPoints} />
-                </svg>
-            </div>
+            <Editor />
 
             {/* Controls */}
-            <div className="lg:min-w-[20rem] p-4 space-y-4 bg-primary-300 text-sm border-primary-600 border-8 border-opacity-50">
-                <SliderTolerance />
-                <SliderStepPoints />
-                {/* <SliderPrecision /> */}
-
-                <div className="flex justify-between">
-                    <ToggleButtons />
-                    <button className="p-1 border border-primary-700 rounded shadow active:scale-[.97]" onClick={() => setPoints([])} title="Clear canvas points">Clear</button>
-                </div>
-            </div>
-
-            {/* In and out points stats */}
-            <div className="col-span-full">
-                <ResultDisplayProduction pointsSrc={points.length} pointsDst={controlPoints.points.length} />
-                {/* <Result pointsSrc={points.length} pointsDst={controlPoints.points.length} /> */}
-            </div>
+            {/* <Controls points={points} setPoints={setPoints} controlPoints={controlPoints} /> */}
         </div>
     );
 };
